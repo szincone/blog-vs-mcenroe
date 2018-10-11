32e13ac9aa27f4a4b0ffa9d3b38f6d1be929bcf2
szincone <sawyerzincone@gmail.com>
2018-10-11 14:41:21 -0600

    added basic color schemes to containers

--- client/tennis-blog-client/src/components/Post.js
+++ client/tennis-blog-client/src/components/Post.js
@@ -9,13 +9,16 @@ const divStyle = {
   textAlign: "center",
   margin: "1rem",
   fontSize: "1.5rem",
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
 };
 
 function Post(props) {
   return (
     <div style={divStyle}>
       <h1>Title: {props.title}</h1>
-      <h2>Content: {props.content}</h2>
+      <h2 style={{ margin: "1rem 0" }}>Content: {props.content}</h2>
       <h3>Score: {props.score}</h3>
     </div>
   );
