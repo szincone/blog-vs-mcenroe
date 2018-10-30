59753a408cc0f8379da9530f188da9165488b839
szincone <sawyerzincone@gmail.com>
2018-10-30 10:37:13 -0600

    removed h1 title from main feed

--- client/tennis-blog-client/src/containers/MainFeed.js
+++ client/tennis-blog-client/src/containers/MainFeed.js
@@ -3,16 +3,8 @@ import Post from "../components/Post.js";
 import { NavLink } from "react-router-dom";
 
 function MainFeed(props) {
-  const h1Style = {
-    textAlign: "center",
-    padding: "1rem",
-    fontSize: "2rem",
-    color: "white",
-    textShadow: "#FC0 0px 0 25px",
-  };
   return (
     <div className="main-container">
-      <h1 style={h1Style}>Tennis-Blog</h1>
       <div className="posts-container">
         {props.posts.map(post => (
           <NavLink to={`/all-notes/${post.id}`} key={post.id}>
