af8cf24bb69c3e744dd7579d9190eedc9dad34b7
szincone <sawyerzincone@gmail.com>
2018-11-01 15:07:16 -0600

    added new_post action and switch to the rootreducer

--- client/tennis-blog-client/src/reducers/index.js
+++ client/tennis-blog-client/src/reducers/index.js
@@ -3,6 +3,7 @@ import {
   FETCH_SUCCESS,
   FETCH_FAILURE,
   FETCHING_POST,
+  ADD_NEW_POST,
   DEL_POST,
   DELETED_POST,
 } from "../actions";
@@ -40,17 +41,24 @@ export const rootReducer = (state = initialState, action) => {
         isFetched: false,
         hasError: action.payload,
       };
-    case DEL_POST: 
+    case DEL_POST:
       return {
         ...state,
-        isDeleting: true
-      }
+        isDeleting: true,
+      };
     case DELETED_POST:
       return {
         ...state,
         isDeleting: false,
-        posts: state.posts.filter(post => post.id !== action.payload)
-      }
+        posts: state.posts.filter(post => post.id !== action.payload),
+      };
+    case ADD_NEW_POST:
+      return {
+        ...state,
+        smurfs: action.payload,
+        isFetching: false,
+        isFetched: true,
+      };
     default:
       return state;
   }
