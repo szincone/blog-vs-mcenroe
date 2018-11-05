fc90a0abeb1a8b53237cff3ba03fbce6a0f33a95
szincone <sawyerzincone@gmail.com>
2018-11-05 12:04:51 -0700

    got rid of logger middleware in redux store

--- client/tennis-blog-client/src/index.js
+++ client/tennis-blog-client/src/index.js
@@ -8,7 +8,8 @@ import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import { rootReducer } from "./reducers";
 import { BrowserRouter as Router } from "react-router-dom";
-const store = createStore(rootReducer, applyMiddleware(thunk, logger));
+// const store = createStore(rootReducer, applyMiddleware(thunk, logger));
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <Provider store={store}>
